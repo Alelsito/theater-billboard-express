e9e2997d7f2089e750276234ef8eeb97c3360f79
Alelsito <lbalelxander@gmail.com>
2022-11-09 19:45:25 -0600

    feat: get all posters of specific play

--- src/routes/plays.js
+++ src/routes/plays.js
@@ -4,11 +4,13 @@ const router = express.Router()
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
+// Get all plays
 router.get('/', async (req, res, next) => {
   const plays = await prisma.play.findMany()
   res.send({ data: plays })
 })
 
+// Get specific play (by :id)
 router.get('/:id', async (req, res, next) => {
   const play = await prisma.play.findUnique({
     where: {
@@ -18,14 +20,16 @@ router.get('/:id', async (req, res, next) => {
   res.send({ data: play })
 })
 
+// Create play
 router.post('/', async (req, res, next) => {
-  const newPlay = await prisma.play.create({ // INSERT
+  const newPlay = await prisma.play.create({
     data: req.body
   })
 
   res.status(201).json(newPlay)
 })
 
+// Update data of specific play (by :id)
 router.patch('/:id', async (req, res, next) => {
   const updatePlay = await prisma.play.update({
     where: {
@@ -37,6 +41,7 @@ router.patch('/:id', async (req, res, next) => {
   res.status(200).json(updatePlay)
 })
 
+// Delete play (by :id)
 router.delete('/:id', async (req, res, next) => {
   const deletePlay = await prisma.play.delete({
     where: {
@@ -45,6 +50,25 @@ router.delete('/:id', async (req, res, next) => {
   })
 
   res.status(200).json(deletePlay)
+})
+
+// Get all posters of specific play
+router.get('/:id/poster', async (req, res, next) => {
+  const play = await prisma.play.findUnique({
+    where: {
+      id: parseInt(req.params.id)
+    }
+  })
+
+  const posters = await prisma.play_poster.findMany({
+    where: {
+      play_id: parseInt(req.params.id)
+    }
+  })
+
+  play.posters = posters
+
+  res.send({ data: { play } })
 })
 
 module.exports = router
