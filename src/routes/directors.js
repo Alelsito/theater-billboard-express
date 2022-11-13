426ccaaede44a0af634d16154d8f6f9f95709004
Alelsito <lbalelxander@gmail.com>
2022-11-12 20:36:42 -0600

    refactor: move and update play_director paths from plays.js to directors.js

--- src/routes/directors.js
+++ src/routes/directors.js
@@ -4,11 +4,15 @@ const router = express.Router()
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
+// CRUD *************
+
+// Get all directors
 router.get('/', async (req, res, next) => {
   const directors = await prisma.director.findMany()
   res.send({ data: directors })
 })
 
+// Get specific director
 router.get('/:id', async (req, res, next) => {
   const director = await prisma.director.findUnique({
     where: {
@@ -18,6 +22,7 @@ router.get('/:id', async (req, res, next) => {
   res.send({ data: director })
 })
 
+// Create director
 router.post('/', async (req, res, next) => {
   const newDirector = await prisma.director.create({ // INSERT
     data: req.body
@@ -26,6 +31,7 @@ router.post('/', async (req, res, next) => {
   res.status(201).json(newDirector)
 })
 
+// Update data of specific director
 router.patch('/:id', async (req, res, next) => {
   const updateDirector = await prisma.director.update({
     where: {
@@ -37,6 +43,7 @@ router.patch('/:id', async (req, res, next) => {
   res.status(200).json(updateDirector)
 })
 
+// Delete director
 router.delete('/:id', async (req, res, next) => {
   const deleteDirector = await prisma.director.delete({
     where: {
@@ -45,6 +52,82 @@ router.delete('/:id', async (req, res, next) => {
   })
 
   res.status(200).json(deleteDirector)
+})
+
+// PLAY_DIRECTOR ----------------
+
+// Get all directors of specific play
+router.get('/play/:id', async (req, res, next) => {
+  const play = await prisma.play.findUnique({
+    where: {
+      id: parseInt(req.params.id)
+    }
+  })
+
+  const directors = await prisma.play_director.findMany({
+    where: {
+      play_id: parseInt(req.params.id)
+    },
+    select: {
+      director: true
+    }
+  })
+
+  play.directors = directors
+
+  res.send({ data: { play } })
+})
+
+// Get specific play_director
+router.get('/:directorId/play/:playId', async (req, res, next) => {
+  const playDirector = await prisma.play_director.findFirst({
+    where: {
+      play_id: parseInt(req.params.playId),
+      director_id: parseInt(req.params.directorId)
+    },
+    include: {
+      director: true
+    }
+  })
+
+  res.send({ data: { playDirector } })
+})
+
+// Insert into play_director
+router.post('/:directorId/play/:playId', async (req, res, next) => {
+  const newPlayDirector = await prisma.play_director.create({
+    data: {
+      play_id: parseInt(req.params.playId),
+      director_id: parseInt(req.params.directorId)
+    }
+  })
+
+  res.status(201).json(newPlayDirector)
+})
+
+// Update play_director
+router.patch('/:directorId/play/:playId', async (req, res, next) => {
+  const updatePlayDirector = await prisma.play_director.updateMany({
+    where: {
+      play_id: parseInt(req.params.playId),
+      director_id: parseInt(req.params.directorId)
+    },
+    data: req.body
+  })
+
+  res.status(200).json(updatePlayDirector)
+})
+
+// Delete specific play_director
+router.delete('/:directorId/play/:playId', async (req, res, next) => {
+  const deletePlayDirector = await prisma.play_director.findFirst({
+    where: {
+      play_id: parseInt(req.params.playId),
+      director_id: parseInt(req.params.directorId)
+    }
+  })
+
+  res.status(200).json(deletePlayDirector)
 })
 
 module.exports = router
