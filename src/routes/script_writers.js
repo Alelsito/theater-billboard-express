0c91e47c4af7ca34d09579703d831428ed3c4a96
Alelsito <lbalelxander@gmail.com>
2022-11-13 20:39:01 -0600

    refactor: move and update play_script_writer from plays.js to script_writers.js

--- src/routes/script_writers.js
+++ src/routes/script_writers.js
@@ -4,11 +4,15 @@ const router = express.Router()
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
+// CRUD *************
+
+// Get all genres
 router.get('/', async (req, res, next) => {
   const scriptWriters = await prisma.script_writer.findMany()
   res.send({ data: scriptWriters })
 })
 
+// Get specific genre
 router.get('/:id', async (req, res, next) => {
   const scriptWriter = await prisma.script_writer.findUnique({
     where: {
@@ -18,6 +22,7 @@ router.get('/:id', async (req, res, next) => {
   res.send({ data: scriptWriter })
 })
 
+// Create genre
 router.post('/', async (req, res, next) => {
   const newScriptWriter = await prisma.script_writer.create({ // INSERT
     data: req.body
@@ -26,6 +31,7 @@ router.post('/', async (req, res, next) => {
   res.status(201).json(newScriptWriter)
 })
 
+// Update data of specific genre
 router.patch('/:id', async (req, res, next) => {
   const updateScriptWriter = await prisma.script_writer.update({
     where: {
@@ -37,6 +43,7 @@ router.patch('/:id', async (req, res, next) => {
   res.status(200).json(updateScriptWriter)
 })
 
+// Delete genre
 router.delete('/:id', async (req, res, next) => {
   const deleteScriptWriter = await prisma.script_writer.delete({
     where: {
@@ -45,6 +52,82 @@ router.delete('/:id', async (req, res, next) => {
   })
 
   res.status(200).json(deleteScriptWriter)
+})
+
+// PLAY_SCRIPT_WRITER ----------------
+
+// Get all scriptWriters of specific play
+router.get('/play/:id', async (req, res, next) => {
+  const play = await prisma.play.findUnique({
+    where: {
+      id: parseInt(req.params.id)
+    }
+  })
+
+  const scriptWriters = await prisma.play_script_writer.findMany({
+    where: {
+      play_id: parseInt(req.params.id)
+    },
+    select: {
+      script_writer: true
+    }
+  })
+
+  play.scriptWriters = scriptWriters
+
+  res.send({ data: { play } })
+})
+
+// Get specific play_script_writer
+router.get('/:scriptwriterId/play/:playId', async (req, res, next) => {
+  const playScriptWriter = await prisma.play_script_writer.findFirst({
+    where: {
+      play_id: parseInt(req.params.playId),
+      script_writer_id: parseInt(req.params.scriptwriterId)
+    },
+    include: {
+      script_writer: true
+    }
+  })
+
+  res.send({ data: { playScriptWriter } })
+})
+
+// Insert into play_script_writer
+router.post('/:scriptwriterId/play/:playId', async (req, res, next) => {
+  const newPlayScriptWriter = await prisma.play_script_writer.create({
+    data: {
+      play_id: parseInt(req.params.playId),
+      script_writer_id: parseInt(req.params.scriptwriterId)
+    }
+  })
+
+  res.status(201).json(newPlayScriptWriter)
+})
+
+// Update play_script_writer
+router.patch('/:scriptwriterId/play/:playId', async (req, res, next) => {
+  const updatePlayScriptWriter = await prisma.play_script_writer.updateMany({
+    where: {
+      play_id: parseInt(req.params.playId),
+      script_writer_id: parseInt(req.params.scriptwriterId)
+    },
+    data: req.body
+  })
+
+  res.status(200).json(updatePlayScriptWriter)
+})
+
+// Delete specific play_script_writer
+router.delete('/:scriptwriterId/play/:playId', async (req, res, next) => {
+  const deletePlayScriptWriter = await prisma.play_script_writer.findFirst({
+    where: {
+      play_id: parseInt(req.params.playId),
+      script_writer_id: parseInt(req.params.scriptwriterId)
+    }
+  })
+
+  res.status(200).json(deletePlayScriptWriter)
 })
 
 module.exports = router
